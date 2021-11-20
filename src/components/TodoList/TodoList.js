7346f9c95c97c37f541eeaee009ae91179a83aff
YaVladimir <dezertir161@gmail.com>
2021-11-20 15:23:37 +0300

    Implemented the functionality of applying filters

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,11 +2,26 @@ import { useSelector } from 'react-redux'
 import React from 'react'
 import { TodoListItem } from '../TodoListItem/TodoListItem'
 import { List } from '@mui/material'
+import { Status } from '../../feature/filter/filterSlice'
 
 export function TodoList() {
   const filter = useSelector((state) => state.filters)
-  console.log(filter);
-  const todos = useSelector((state) => state.todos)
+  console.log(filter)
+  const todos = useSelector((state) => {
+    let filteredTodos = state.todos
+    if (filter.status === Status.Active) {
+      filteredTodos = state.todos.filter((todo) => !todo.completed)
+    } else if (filter.status === Status.Completed) {
+      filteredTodos = state.todos.filter((todo) => todo.completed)
+    }
+    if (filter.colors.length !== 0) {
+      filteredTodos = filteredTodos.filter((todo) =>
+        filter.colors.includes(todo.color)
+      )
+    }
+    return filteredTodos
+  })
+  console.log(todos)
   const list = todos.map((todo) => {
     return <TodoListItem id={todo.id} todo={todo} />
   })
